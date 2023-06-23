7dcfac8b7c69d46c040a8cd2600e98530f905747
Enrique Barbosa <e.barbosa@grouplinknetwork.com>
2023-06-23 11:20:02 -0300

    New routes added in MainRoutes

--- src/routes/mainRoutes.tsx
+++ src/routes/mainRoutes.tsx
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const MainRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<div>Teste</div>} />
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 };
