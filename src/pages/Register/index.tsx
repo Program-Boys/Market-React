d07fe38c38b3898870ca8f6f7f6340a255366551
Guilherme <guilhermeshadow91@gmail.com>
2023-07-06 17:41:14 -0300

    adding registerUser function in userContext

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,4 +1,11 @@
+import { useContext } from "react";
+import { UserContext } from "../../context/User.context";
+
 const Register = () => {
+  const { registerUser } = useContext(UserContext);
+
+  console.log("CHEGOU MENÓ", registerUser);
+
   return <></>;
 };
 
