f41976c5caaa3f184ee5a3ceba62e8469380961f
Enrique Barbosa <e.barbosa@grouplinknetwork.com>
2023-07-06 23:43:40 -0300

    Structure changed

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,12 +1,15 @@
 import { useContext } from "react";
 import { UserContext } from "../../context/User.context";
+import Form from "../../components/Form";
 
 const Register = () => {
   const { registerUser } = useContext(UserContext);
 
-  console.log("CHEGOU MENÓ", registerUser);
-
-  return <></>;
+  return (
+    <Form accountSubmit={() => {}}>
+      <input type="text" />
+    </Form>
+  );
 };
 
 export default Register;
