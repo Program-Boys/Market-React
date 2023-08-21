d2ea183844f0eb4d140b4861ef9bafe930aa4ef3
Enrique Barbosa <e.barbosa@grouplinknetwork.com>
2023-08-21 14:53:00 -0300

    Changed component

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,6 +1,5 @@
 import { useContext } from "react";
 import { UserContext } from "../../context/User.context";
-import Form from "../../components/Form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import { registerFormSchema } from "../../validations/form.validations";
@@ -20,10 +19,6 @@ const Register = () => {
   return (
     <Container>
       <h1>Register Page</h1>
-      <Form accountSubmit={() => {}}>
-        <input type="text" />
-        <input type="email" />
-      </Form>
     </Container>
   );
 };
